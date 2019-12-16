6aabf7344096b2ba7363d8e206ef0fa405826c3d
Michaël De Boey <info@michaeldeboey.be>
2019-12-16 21:54:13 +0100

    Only load Instagram script if there's an embed code on the page

--- src/gatsby-browser.js
+++ src/gatsby-browser.js
@@ -1,11 +1,13 @@
 exports.onRouteUpdate = () => {
-  // Wait to ensure page is rendered first.
-  setTimeout(() => {
-    if (
-      typeof gatsbyLoadInstagram !== `undefined` &&
-      typeof window.gatsbyLoadInstagram === `function`
-    ) {
-      window.gatsbyLoadInstagram();
-    }
-  }, 0);
+  if (document.querySelector('.instagram-media') !== null) {
+    // Wait to ensure page is rendered first.
+    setTimeout(() => {
+      if (
+        typeof gatsbyLoadInstagram !== `undefined` &&
+        typeof window.gatsbyLoadInstagram === `function`
+      ) {
+        window.gatsbyLoadInstagram();
+      }
+    }, 0);
+  }
 };
