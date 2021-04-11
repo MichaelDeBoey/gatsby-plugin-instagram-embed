e7d4cb19f39a97b8b6f08b4d12f006eb369933fd
Michaël De Boey <info@michaeldeboey.be>
2021-04-11 17:45:31 +0200

    feat: support Gatsby 3.x
    BREAKING CHANGE: Requires Node@>=12.13.x
    BREAKING CHANGE: Requires Gatsby@^3.x

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -1,3 +1,1 @@
-if (process.env.GATSBY_EXPERIMENTAL_PLUGIN_OPTION_VALIDATION) {
-  exports.pluginOptionsSchema = ({ Joi }) => Joi.object({});
-}
+exports.pluginOptionsSchema = ({ Joi }) => Joi.object({});
